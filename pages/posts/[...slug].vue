d0ea3d52768b11a8d14e1782b98f46468f328389
Enzo Jimenez <enzo-jimenez@hotmail.com>
2023-10-20 18:36:19 -0300

    fix style posts

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
     <div class="leading-normal tracking-normal">
         <div class="max-w-6xl mx-auto fondo-image" :style="`background-image:url('${data.image.src}');`"></div>
-        <div class="container max-w-5xl mx-auto md:-mt-32">
+        <div class="container max-w-8xl mx-auto md:-mt-32">
             <div class="mx-0 mb-8 sm:mx-6">
                 <div class="bg-slate-100 dark:bg-gray-800 w-full p-8 md:p-24 text-xl md:text-2xl rounded-lg">
                     <ContentDoc class="prose prose-headings:no-underline md:prose-lg lg:prose-x max-w-none dark:prose-invert" />
