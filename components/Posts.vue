28812b5ef63dac5ef23b5ad5e3fcb285beaf6b5d
Enzo Jimenez <enjimenez@crjppf.gov.ar>
2023-10-20 14:03:51 -0300

    fix bg index y hover en posts

--- components/Posts.vue
+++ components/Posts.vue
@@ -2,7 +2,7 @@
     <p class="text-xl md:text-2xl text-gray pt-10 uppercase text-center">Lessons</p>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div v-for="post in posts" :key="post._path" class="col-auto py-3 flex flex-col flex-grow flex-shrink">
-            <div class="flex-1 rounded overflow-hidden bg-white dark:bg-gray-800 shadow-lg border-solid border-2 border-gray-600">
+            <div class="flex-1 rounded overflow-hidden bg-slate-100 dark:bg-slate-800 shadow hover:shadow-lg hover:bg-slate-200  dark:hover:bg-slate-600">
                 <NuxtLink :to="post._path" class="flex flex-wrap no-underline hover:no-underline">
                     <img :src="post.image.src" class="h-64 w-full rounded-t pb-6">
                     <div class="w-full font-bold text-xl px-6">{{ post.title }}</div>
--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,9 @@
 
 		<div class="container px-4 md:px-0 max-w-6xl mx-auto -mt-32">
 			<div class="mx-0 sm:mx-6">
-				<div class="bg-gray-200 dark:bg-gray-700 p-4 w-full text-xl md:text-2xl text-gray-800 dark:text-gray-300 leading-normal rounded-t">
+				<div class="bg-gray-100 dark:bg-gray-700 p-4 w-full leading-normal rounded-t">
 					<div class="flex h-full bg-white rounded overflow-hidden shadow-lg">
-						<div class="flex-1 bg-white dark:bg-gray-800 rounded-t rounded-b-none overflow-hidden shadow-lg p-5">
+						<div class="flex-1 bg-gray-200 dark:bg-gray-800 rounded-t rounded-b-none overflow-hidden shadow-lg p-5">
 							<ContentRenderer class="prose max-w-none dark:prose-invert" :value="data"/>
 						</div>
 					</div>
